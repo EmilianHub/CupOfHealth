d6f3e6c9f48dcbd1767de1b9d2cb06350f889c13
Kochanek_Emilian <Kochanek.Emilian@gmail.com>
2023-03-10 19:24:16 +0100

    ER CUP-2 Password restart

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,12 @@
-from flask import Flask, request
-from backend.userManagement.passwordRestart import PasswordRestart
+from flask import Flask
+from backend.userManagement.userResource import user
 
 app = Flask(__name__)
+app.register_blueprint(user, url_prefix="/user")
 
 @app.get('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
-
-
-@app.post("/")
-def create_user():
-    args = request.get_json()
-    name = args.get("name")
-    password = args.get("password")
-    return f"{name} and {password}"
-
-
-@app.post("/password/restart")
-def restartUserPassword():
-    passwordRestart = PasswordRestart()
-    args = request.get_json()
-    email = args.get("email")
-    return passwordRestart.tryRestartPassword(email)
 
 
 if __name__ == '__main__':
